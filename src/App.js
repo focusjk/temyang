aa6818b41486ed5044b11691eb55a16b0d16fa90
Admin <admin@MacBook-Pro-khxng-Admin.local>
2018-02-05 23:30:56 +0700

    Add UI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
 
 class App extends Component {
   render() {
+    let nums = [];
+    for(var i = 0; i < 12; i++) {
+      nums.push(i);
+    }
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          
+          <h1 className="App-title">Temyang</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          nums.map((key) => {
+            return <div className="card" >co-working {key + 1}</div>
+          })
+        }
       </div>
     );
   }
