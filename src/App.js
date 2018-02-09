6ed73f1297cabe0efa86bb4c57b31733f82574be
goodkavin <goodkavin@gmail.com>
2018-02-09 20:08:47 +0700

    progress bar with color

--- src/App.js
+++ src/App.js
@@ -10,39 +10,37 @@ class App extends Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
       percent: 0,
     };
-    this.increase = this.increase.bind(this);
-    this.restart = this.restart.bind(this);
+    this.show = this.show.bind(this);
   }
 
   componentDidMount() {
-    this.increase();
+
   }
 
-  increase() {
-    const percent = this.state.percent + 1;
-    if (percent >= 100) {
-      clearTimeout(this.tm);
-      return;
-    }
-    this.setState({ percent });
-    this.tm = setTimeout(this.increase, 10);
-  }
+  show(x,id) { 
+      this.setState({selected: id, percent: x});
+      if(x>=75){
+        this.setState({color: '#FF260F'});
+      }else if(x>=25){
+        this.setState({color: '#FFCD1B'});
+      }else{
+        this.setState({color: '#1DFF26'});
+      }
 
-  restart() {
-    clearTimeout(this.tm);
-    this.setState({ percent: 0 }, () => {
-      this.increase();
-    });
   }
 
   render() {
-    const data = [{"name":"Sheldon Ekamai 13","img":"http://drive.google.com/uc?=view&id=1T7iy454QOXyHpMqKyQYrTE0JhNtV69wc"},
-                  {"name":"Starbucks 1st fl Paragon","img":"http://drive.google.com/uc?export=view&id=10wYzZMgQMRMY1Xn_NyoO4IkND1LwW1bm"},
-                  {"name":"Starbucks I'm Park","img":"http://drive.google.com/uc?export=view&id=1uzpFGjy1gPn4DiOKuLWL14xGM98iS4LJ"},
-                  {"name":"Starbucks Siam Square One","img":"http://drive.google.com/uc?export=view&id=1VYGVsN95HhfcBLtzP7mMtREzNqQbSn_t"}];
+    const data = [{"name":"Sheldon Ekamai 13","img":"http://drive.google.com/uc?=view&id=1T7iy454QOXyHpMqKyQYrTE0JhNtV69wc","space":"13"},
+                  {"name":"Starbucks 1st fl Paragon","img":"http://drive.google.com/uc?export=view&id=10wYzZMgQMRMY1Xn_NyoO4IkND1LwW1bm","space":"86"},
+                  {"name":"Starbucks I'm Park","img":"http://drive.google.com/uc?export=view&id=1uzpFGjy1gPn4DiOKuLWL14xGM98iS4LJ","space":"43"},
+                  {"name":"Starbucks Siam Square One","img":"http://drive.google.com/uc?export=view&id=1VYGVsN95HhfcBLtzP7mMtREzNqQbSn_t","space":"97"}];
+    
+    const { selected } = this.state
+
     return (
       <div className="App">
 
@@ -53,14 +51,14 @@ class App extends Component {
         {
           data.map((d, idx) => {
             return (
-             <div className="container">
-              <img src={d.img}/>
-                <div className="centered">{d.name}</div>
-                <div style={{ margin: 10, width: 200 }}>
-                  <Circle strokeWidth="6" percent={this.state.percent} />
-                  <Line strokeWidth="4" percent={this.state.percent} />
-                  <button onClick={this.restart}>Restart</button>
-                </div>
+              <div>
+                <div className='container' onClick={() => this.show(d.space,idx)} >
+                <img src={d.img}/>
+                  <div className="centered">{d.name}</div>
+                  <div className="bar" style={{ margin: 0, width: 200 }}>
+                    {selected == idx && <Line strokeWidth="4" percent={this.state.percent} strokeColor={this.state.color}/> }
+                  </div>
+              </div>
             </div>
           )})
         }
