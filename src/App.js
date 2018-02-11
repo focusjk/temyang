bca4546395ec45006a31152d36a5df3914218de9
goodkavin <goodkavin@gmail.com>
2018-02-11 09:24:43 +0700

    Smaller pics + get data from server

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 import { Circle, Line } from 'rc-progress';
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 
@@ -13,12 +13,26 @@ class App extends Component {
 
     this.state = {
       percent: 0,
-    };
+      data:[{"name":"constructor Ekamai 13","img":"http://drive.google.com/uc?=view&id=1T7iy454QOXyHpMqKyQYrTE0JhNtV69wc","space":"13"},
+                  {"name":"Starbucks 1st fl Paragon","img":"http://drive.google.com/uc?export=view&id=10wYzZMgQMRMY1Xn_NyoO4IkND1LwW1bm","space":"86"},
+                  {"name":"Starbucks I'm Park","img":"http://drive.google.com/uc?export=view&id=1uzpFGjy1gPn4DiOKuLWL14xGM98iS4LJ","space":"43"},
+                  {"name":"Starbucks Siam Square One","img":"http://drive.google.com/uc?export=view&id=1VYGVsN95HhfcBLtzP7mMtREzNqQbSn_t","space":"97"}]
+   };
     this.show = this.show.bind(this);
+    console.log(this.state.data)
   }
 
   componentDidMount() {
-
+    axios.get('http://128.199.97.10:3000/coworking/coworkinglist').then((res) => {
+      console.log(res);
+      const res2 = [{"name":"Sheldon Ekamai 13","img":"http://drive.google.com/uc?=view&id=1IkslDiOCDdAHeLJD4uk9Tl5dopwtspIw","space":"13"},
+                  {"name":"Starbucks 1st fl Paragon","img":"http://drive.google.com/uc?export=view&id=1zRQcr2H3Tu6hSQTH0s2O5zXbf9y-5uPT","space":"86"},
+                  {"name":"Starbucks I'm Park","img":"http://drive.google.com/uc?export=view&id=13aeitmqPAyTsoXshrpuyD5hjY9cVB91q","space":"43"},
+                  {"name":"Starbucks Siam Square One","img":"http://drive.google.com/uc?export=view&id=1ewIhLQzIzZaKbRYpQKj77Pqu08Ya4tNn","space":"97"}]
+      this.setState({data: res2})
+    }, (error) => {
+      console.log('get error');
+    })
   }
 
   show(x,id) { 
@@ -34,11 +48,14 @@ class App extends Component {
   }
 
   render() {
-    const data = [{"name":"Sheldon Ekamai 13","img":"http://drive.google.com/uc?=view&id=1T7iy454QOXyHpMqKyQYrTE0JhNtV69wc","space":"13"},
+    const data1 = [{"name":"Sheldon Ekamai 13","img":"http://drive.google.com/uc?=view&id=1T7iy454QOXyHpMqKyQYrTE0JhNtV69wc","space":"13"},
                   {"name":"Starbucks 1st fl Paragon","img":"http://drive.google.com/uc?export=view&id=10wYzZMgQMRMY1Xn_NyoO4IkND1LwW1bm","space":"86"},
                   {"name":"Starbucks I'm Park","img":"http://drive.google.com/uc?export=view&id=1uzpFGjy1gPn4DiOKuLWL14xGM98iS4LJ","space":"43"},
                   {"name":"Starbucks Siam Square One","img":"http://drive.google.com/uc?export=view&id=1VYGVsN95HhfcBLtzP7mMtREzNqQbSn_t","space":"97"}];
     
+    const {data} = this.state
+    console.log(data)
+
     const { selected } = this.state
 
     return (
